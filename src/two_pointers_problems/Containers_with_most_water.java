b69bad78f9f26827e4ee7c9a1466f0937193ef33
AswinBarath <aswin2001barath@gmail.com>
2024-03-01 23:48:35 +0530

    Container With Most Water

---
+++ src/two_pointers_problems/Containers_with_most_water.java
@@ -0,0 +1,20 @@
+package two_pointers_problems;
+
+public class Containers_with_most_water {
+    public int maxArea(int[] A) {
+        int l = 0, r = A.length - 1;
+        long maxAns = 0;
+        while (l < r) {
+            long width = Math.abs(r - l);
+            long height = Math.min(A[l], A[r]);
+            long area = width * height;
+            maxAns = Math.max(maxAns, area);
+            if (A[l] < A[r])
+                l++;
+            else
+                r--;
+            // System.out.print("ans="+area+", ");
+        }
+        return (int) maxAns;
+    }
+}
